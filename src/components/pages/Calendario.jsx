91efe1016de74d0aec77817ebda24ce28d805a7d
Julia <julialopezcalvo95@gmail.com>
2025-02-04 12:41:04 +0100

    calendario

--- src/components/pages/Calendario.jsx
+++ src/components/pages/Calendario.jsx
@@ -11,6 +11,34 @@ function Calendario(){
             <h1 className="Calenderhero__h1">Calendario</h1>
 
         </div>
+
+        <section className="InfoSec">
+            <ul className="InfoSec__ul">
+                <li className="InfoSec__li">
+                    <button className="InfoSec__btn1"></button>
+                    <p>Horario normal </p>
+                </li>
+
+                <li className="InfoSec__li">
+                    <button className="InfoSec__btn2"></button>
+                    <p>Hueco libre para recuperar</p>
+                </li>
+
+                <li className="InfoSec__li">
+                    <button className="InfoSec__btn3"></button>
+                    <p>Festivos/Cierres</p>
+                </li>
+
+                <li className="InfoSec__li">
+                    <button className="InfoSec__btn4"></button>
+                    <p>Clases Recuperadas</p>
+                </li>
+            </ul>
+        </section>
+
+        <section className="horario">
+            <h2 className="horario__h2">Hora</h2>
+        </section>
         </div>
         
 
