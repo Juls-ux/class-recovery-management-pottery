218fc71c9c33657fb31a24ffb75adfa24d563810
Julia <julialopezcalvo95@gmail.com>
2025-02-19 12:58:04 +0100

    add new alumna a la lista

--- src/components/Form/Preview.jsx
+++ src/components/Form/Preview.jsx
@@ -8,17 +8,17 @@ function Preview({newAlumna = {} }) {
         <h3 className="preview--title">Vista Previa de la Información de la Alumna</h3>
 
         {newAlumna.nombre ? (
-          <ul className="listado__ul">
-            <li className="listado__li">
-              <p className="listado__addInput">{newAlumna.nombre || "Nombre"}</p>
-              <p className="listado__addInput">{newAlumna.email || "Email"}</p>
-              <p className="listado__addInput">{newAlumna.telefono || "Teléfono"}</p>
-              <p className="listado__addInput">{newAlumna.dia || "Día"}</p>
-              <p className="listado__addInput">{newAlumna.horario || "Hora"}</p>
+          <ul className="preview__ul">
+            <li className="preview__li">
+              <p className="preview__addInput">{newAlumna.nombre || "Nombre"}</p>
+              <p className="preview__addInput">{newAlumna.email || "Email"}</p>
+              <p className="preview__addInput">{newAlumna.telefono || "Teléfono"}</p>
+              <p className="preview__addInput">{newAlumna.dia || "Día"}</p>
+              <p className="preview__addInput">{newAlumna.horario || "Hora"}</p>
             </li>
           </ul>
         ) : (
-          <p>No hay información de la alumna disponible.</p>
+          <p className="preview__p" >No hay información de la alumna disponible.</p>
         )}
       </section>
     </div>
