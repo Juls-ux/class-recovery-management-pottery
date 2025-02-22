715cd794710d51b26bf1a5b39cd50d2c1330e506
Julia <julialopezcalvo95@gmail.com>
2025-02-22 10:11:57 +0100

    add list im groups

--- src/components/pages/Grupos.jsx
+++ src/components/pages/Grupos.jsx
@@ -1,56 +1,76 @@
-
-import { useState } from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
 function Grupos({ searchTerm, setSearchTerm, alumnosAsignados, setAlumnosAsignados, alumnosAsignadosGrupo }) {
 
+    console.log("Datos de alumnosAsignadosGrupo:", alumnosAsignadosGrupo);
 
     const dias = [...new Set(alumnosAsignadosGrupo.map(obj => obj.dia))];
     const horario = [...new Set(alumnosAsignadosGrupo.map(obj2 => obj2.horario))];
 
+    console.log("Días:", dias);
+    console.log("Horarios:", horario);
+
     return (
-        <>
-            <div className="grupos">
-                <h1>Visión Global de Grupos</h1>
+        <div className="grupos">
+            <h1>Visión Global de Grupos</h1>
 
-                {/* Barra de búsqueda y botones */}
-                <div className="grupos__acciones">
-                    <input className="grupos__input" type="search" placeholder="Buscar" />
-                    <button className="grupos__add-btn">Añadir</button>
-                    <button className="grupos__manage-btn">Gestión Alumnos</button>
-                </div>
-
-                {/* Tabla de horarios */}
-                <section className="tabla-container">
-                    {/* Primera celda vacía */}
-                    <div className="tabla__header tabla__empty"></div>
-
-                    {/* Días de la semana en la parte superior */}
-                    {dias.map((dia) => (
-                        <div key={dia} className="tabla__header">
-                            {dia}
-                        </div>
-                    ))}
-
-                    {/* Horarios y celdas vacías */}
-                    {horario.map((hora) => (
-                        <>
-                            <div key={hora} className="tabla__horario">{hora}</div>
-                            {dias.map((dia, index) => (
-                                <div key={`${hora}-${dia}-${index}`} className="tabla__cell"></div>
-                            ))}
-                        </>
-                    ))}
-                </section>
+            <div className="grupos__acciones">
+                <input
+                    className="grupos__input"
+                    type="search"
+                    placeholder="Buscar"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button className="grupos__add-btn">Añadir</button>
+                <button className="grupos__manage-btn">Gestión Alumnos</button>
             </div>
-        </>
-    )
+
+            <section className="tabla-container">
+                {alumnosAsignados.length > 0 ? (
+                    <>
+
+                        <div className="tabla__header tabla__empty"></div>
+
+                        {dias.map((dia) => (
+                            <div key={dia} className="tabla__header">{dia}</div>
+                        ))}
+
+                        {horario.map((hora) => (
+                            <React.Fragment key={hora}>
+                                <div className="tabla__horario">{hora}</div>
+                                {dias.map((dia) => (
+
+                                    <div key={`${hora}-${dia}`} className="tabla__cell">
+
+                                        {alumnosAsignadosGrupo
+                                            .filter(alumno => alumno.dia === dia && alumno.horario === hora)
+                                            .map((alumno) => (
+                                                <div key={alumno.id}>
+                                                    <li>{alumno.nombre}</li>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                ))}
+                            </React.Fragment>
+                        ))}
+                    </>
+                ) : (
+                    <div className="tabla__mensaje">No hay alumnos disponibles.</div>
+                )}
+            </section>
+        </div>
+    );
 }
+
 Grupos.propTypes = {
-    searchTerm: PropTypes.func,
+    searchTerm: PropTypes.string,
     setSearchTerm: PropTypes.func,
     setAlumnosAsignados: PropTypes.func,
     alumnosAsignados: PropTypes.array,
+    alumnosAsignadosGrupo: PropTypes.array.isRequired
 };
 
 export default Grupos;
